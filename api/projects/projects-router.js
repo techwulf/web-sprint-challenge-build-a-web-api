b96b30ba0c29a5d99a406c2facb426cbd1631513
techwulf <t.zach.moore@gmail.com>
2021-10-01 11:14:09 -0600

    implement get /api/projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,5 +1,15 @@
 const express = require('express');
 
+const Projects = require('./projects-model');
+
 const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  Projects.get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(next);
+});
 
 module.exports = router;
